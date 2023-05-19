6cda6575454fbd2b90048a0ead60c8574275d84c
raptatinha <raptatinha@users-MacBook-Pro.local>
2023-05-18 17:09:49 -0700

    Clean up code

--- bdd-tests/step-definition/example.step.ts
+++ bdd-tests/step-definition/example.step.ts
@@ -3,21 +3,17 @@ import { expect } from '@playwright/test';
 import { ICustomWorld } from '../support/custom-world';
 
 Given('I am at the playwright website', async function (this: ICustomWorld) {
-    const page = this.page!;
-    await page.goto('https://playwright.dev/');
+    await this.page!.goto('https://playwright.dev/');
 });
   
 When('I click at "Get Started"', async function (this: ICustomWorld) {
-    const page = this.page!;
-    await page.getByRole('link', { name: 'Get started' }).click();
+    await this.page!.getByRole('link', { name: 'Get started' }).click();
 });
   
 Then('the title has the text "Playwright"', async function (this: ICustomWorld) {
-    const page = this.page!;
-    await expect(page).toHaveTitle(/Playwrights/);//this line is intentionaly failing so we can see the reports
+    await expect(this.page!).toHaveTitle(/Playwrights/);//this line is intentionally failing so we can see the reports
 });
 
 Then('the URL has the text "intro"', async function (this: ICustomWorld) {
-    const page = this.page!;
-    await expect(page).toHaveURL(/.*intro/);
+    await expect(this.page!).toHaveURL(/.*intro/);
 });
